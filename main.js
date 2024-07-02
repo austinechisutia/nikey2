96a97805954bb0a52676f70a1b8f1065b9a43504
austinechisutia <106369577+austinechisutia@users.noreply.github.com>
2024-07-02 17:13:33 +0300

    fourth commit

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+// function myFunction() {
+//   document.getElementById("dropDown").classList.toggle("show");
+// }
+
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
